e142b48bac5b4964ef1ec318ddcab60261f6af31
ionbisa <ionbisa@gmail.com>
2026-05-24 02:42:34 +0700

    update tambahan papan skor

--- main_2.js
+++ main_2.js
@@ -15,15 +15,21 @@ function getHasil(comp, player) {
   if (player == "semut") return comp == "orang" ? "KALAH!" : "MENANG!";
 }
 
+function getPoin(hasil) {
+  if (hasil == "MENANG!") return { player: 3, computer: 0 };
+  if (hasil == "SERI!") return { player: 1, computer: 1 };
+  return { player: 0, computer: 3 };
+}
+
 // menentukan pilihan Computer dengan memutar image
 function putar() {
   const imgComputer = document.querySelector(".img-komputer");
   const gambar = ["gajah", "semut", "orang"];
   let i = 0;
   const waktuMulai = new Date().getTime();
-  setInterval(function () {
+  const intervalId = setInterval(function () {
     if (new Date().getTime() - waktuMulai > 1000) {
-      clearInterval;
+      clearInterval(intervalId);
       return;
     }
     imgComputer.setAttribute("src", "img/" + gambar[i++] + ".png");
@@ -31,13 +37,86 @@ function putar() {
   }, 100);
 }
 
+let skorPlayer = 0;
+let skorComputer = 0;
+let ronde = 1;
+let gameSelesai = false;
+let sedangMemutar = false;
+let gameDimulai = false;
+
+const areaPlayer = document.querySelector(".area-player");
+const scorePlayer = document.querySelector(".score-player");
+const scoreComputer = document.querySelector(".score-computer");
+const roundNumber = document.querySelector(".round-number");
+const startPopup = document.querySelector(".start-popup");
+const popup = document.querySelector(".game-popup");
+const popupTitle = popup.querySelector(".popup-title");
+const popupMessage = popup.querySelector(".popup-message");
+const startGame = document.querySelector(".start-game");
+const playAgain = document.querySelector(".play-again");
+
+function updateScoreboard() {
+  scorePlayer.innerHTML = skorPlayer;
+  scoreComputer.innerHTML = skorComputer;
+
+  if (gameSelesai) {
+    roundNumber.innerHTML = "Selesai";
+    return;
+  }
+
+  if (ronde <= 5) {
+    roundNumber.innerHTML = ronde + "/5";
+    return;
+  }
+
+  roundNumber.innerHTML = ronde;
+}
+
+function cekGameSelesai() {
+  return ronde > 5 && skorPlayer != skorComputer;
+}
+
+function tampilkanPopup() {
+  const pemenang = skorPlayer > skorComputer ? "Player Menang!" : "Computer Menang!";
+  popupTitle.innerHTML = pemenang;
+  popupMessage.innerHTML = "Skor akhir " + skorPlayer + " - " + skorComputer;
+  popup.classList.remove("hidden");
+}
+
+function resetGame() {
+  skorPlayer = 0;
+  skorComputer = 0;
+  ronde = 1;
+  gameSelesai = false;
+  sedangMemutar = false;
+  gameDimulai = true;
+
+  document.querySelector(".info").innerHTML = "";
+  document.querySelector(".img-komputer").setAttribute("src", "img/gajah.png");
+  areaPlayer.classList.remove("disabled");
+  popup.classList.add("hidden");
+  updateScoreboard();
+}
+
+function mulaiGame() {
+  gameDimulai = true;
+  startPopup.classList.add("hidden");
+  areaPlayer.classList.remove("disabled");
+}
+
 //menentukan hasil
 const pilihan = document.querySelectorAll("li img");
 pilihan.forEach(function (pil) {
   pil.addEventListener("click", function () {
+    if (!gameDimulai || gameSelesai || sedangMemutar) return;
+
+    sedangMemutar = true;
+    areaPlayer.classList.add("disabled");
+
     const pilihanComputer = getPilihanComputer();
     const pilihPlayer = pil.className;
     const hasil = getHasil(pilihanComputer, pilihPlayer);
+    const poin = getPoin(hasil);
 
     putar();
     setTimeout(function () {
@@ -46,6 +125,25 @@ pilihan.forEach(function (pil) {
 
       const info = document.querySelector(".info");
       info.innerHTML = hasil;
+
+      skorPlayer += poin.player;
+      skorComputer += poin.computer;
+      ronde++;
+
+      if (cekGameSelesai()) {
+        gameSelesai = true;
+        updateScoreboard();
+        tampilkanPopup();
+        return;
+      }
+
+      sedangMemutar = false;
+      areaPlayer.classList.remove("disabled");
+      updateScoreboard();
     }, 1000);
   });
 });
+
+areaPlayer.classList.add("disabled");
+startGame.addEventListener("click", mulaiGame);
+playAgain.addEventListener("click", resetGame);
